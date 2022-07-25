aa164d68e4cfcee9136878a0d410a3e97c32b93a
Nabil Tharwat <example@example.com>
2022-07-25 17:08:08 +0200

    feat: initial release
    Signed-off-by: Nabil Tharwat <example@example.com>

---
+++ README.md
@@ -0,0 +1,41 @@
+## Introduction
+
+> Hello there~ https://youtu.be/eaEMSKzqGAg
+
+A simple command line utility to help you initialize git repositories locally!
+
+I have multiple gpg keys and git emails on my machine and it was tedious to clone,
+configure git, and make sure everything works every time. This package makes my
+life much easier. The NPM package is just a wrapped around my script, you can
+copy it, modify it however you like, the limit's the sky!
+
+## Installation
+
+```
+npm i -g initrepo
+# or
+yarn global add initrepo
+```
+
+Why global? Because you want to run it anywhere.
+
+## Features
+
+- Choose between creating a directory for the repo or initializing in `cwd`
+- Lists gpg keys interactively for you to choose
+- Specify commit author email
+- Automatically add a remote
+
+## Supported Operating Systems
+
+I tested this on the latest Windows 10 build. It _should_ work on other versions
+of windows AND linux distros with NodeJS > 12 installed.
+
+## Contributing
+
+I welcome all contributions! Feel free to open an issue or submit a PR! 😀
+
+## License
+
+This project is licensed under MIT. Refer to [LICENSE](LICENSE) for more
+details.
---
+++ main.js
@@ -0,0 +1,109 @@
+#!/usr/bin/env node
+
+const inquirer = require("inquirer");
+const path = require("path");
+const { execSync } = require("child_process");
+const { mkdirSync } = require("fs");
+
+const gpgKeyRegex =
+	/sec\s+rsa(2048|4096)\/(?<key>[A-Za-z0-9]+) .+\n .+\n.+(?<email><.+>)/g;
+const commands = {
+	gpgListKeys: "gpg --list-secret-keys --keyid-format=long",
+	init: "git init",
+	setGpgSigningOn: `git config commit.gpgSign true`,
+	setRemote: (remote) => `git remote add origin ${remote}`,
+	setUserEmail: (email) => `git config user.email ${email}`,
+	setGpgKey: (key) => `git config user.signingkey ${key}`,
+};
+
+const gpgKeysOutput = execSync(commands.gpgListKeys).toString();
+const matches = gpgKeysOutput.matchAll(gpgKeyRegex);
+const gpgKeys = [];
+
+for (const match of matches) {
+	gpgKeys.push({
+		key: match.groups["key"],
+		email: match.groups["email"],
+	});
+}
+
+inquirer
+	.prompt([
+		{
+			type: "input",
+			name: "name",
+			message:
+				"What is the repo name? (in lowercase, keep this empty to init in cwd)",
+			default: "",
+			validate: (input) =>
+				input.trim().length === 0 ? "Please enter a valid name" : true,
+		},
+		{
+			type: "list",
+			name: "user.signingKey",
+			message: "Which gpg key do you want to use with this repo?",
+			choices: gpgKeys.map((key) => ({
+				name: `${key.key} - ${key.email}`,
+				value: key.key,
+			})),
+		},
+		{
+			type: "input",
+			name: "user.email",
+			message: "What is the author email for this repo?",
+			default: "example@example.com",
+		},
+		{
+			type: "input",
+			name: "remote",
+			message: "What's the repo's remote? (Ignore if none exist)",
+			default: "",
+		},
+		{
+			type: "confirm",
+			name: "confirm",
+			message: (answers) => {
+				const location = !answers.name ? "cwd" : answers.name;
+
+				return `You're initializing in ${location} with the data highlighted above, are you sure?`;
+			},
+			default: "",
+		},
+	])
+	.then((answers) => {
+		if (!answers.confirm) {
+			console.log(
+				"🛑 Terminating. You may run the utility again to restart initialization."
+			);
+			return;
+		}
+
+		const directory = answers.name
+			? path.resolve(process.cwd(), answers.name)
+			: process.cwd();
+
+		if (answers.name) {
+			mkdirSync(answers.name);
+			process.chdir(directory);
+		}
+
+		console.log("🚌 Initializing your repo, just a sec!");
+
+		execSync(commands.init);
+		execSync(commands.setGpgSigningOn);
+		execSync(commands.setUserEmail(answers.user.email));
+		execSync(commands.setGpgKey(answers.user.signingKey));
+
+		if (answers.remote) execSync(commands.setRemote(answers.remote));
+
+		console.log(
+			"✅ All done. You may run `git fetch origin` whenever you wish to pull the repo. Enjoy! 🎉"
+		);
+	})
+	.catch((error) => {
+		if (error.isTtyError) {
+			console.log("🛑 Prompt couldn't be rendered in the current environment");
+		} else {
+			console.log(error);
+		}
+	});
